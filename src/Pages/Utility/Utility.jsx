b2408ac185c6f7e4931955bedd367ec274aabe83
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-03-15 19:53:07 +0500

    first commit

--- src/Pages/Utility/Utility.jsx
+++ src/Pages/Utility/Utility.jsx
@@ -1,53 +1,54 @@
 import React from "react";
 import "./Utility.css";
+import img from "../../Assets/util.png";
 function Utility() {
   return (
-    <div id="util" className="story-cont">
+    <div id="util" className="util-cont">
       <div className="util">
-        <h1>Sol Steaks Utility</h1>
-        <div className="cards-div">
-          <div className="card">
-            <h2 className="sub">
-              Foodie, Chill Out Manager, Star, Celeb Head Chef
-            </h2>
-            <ul className="story-div">
-              <li>Collect $WAGYU & exchange it for your Steaks House.</li>
-              <li>Use $WAGYU to decorate your Steak House.</li>
-            </ul>
-          </div>
-          <div className="card">
-            <h2 className="sub">Rancher</h2>
-            <ul className="story-div">
-              <li>Collect $WAGYU & exchange it for your Farm.</li>
-              <li>Use $WAGYU to decorate your Farm.</li>
-            </ul>
-          </div>
-          <div className="card">
-            <h2 className="sub">IRL Utility</h2>
-            <ul className="story-div">
-              <li>$WAGYU as only ticket to IRL events.</li>
-              <li>Discounts at selected Steaks Houses</li>
-            </ul>
-          </div>
-          <div className="card">
-            <h2 className="sub">$WAGYU Merch</h2>
-            <ul className="story-div">
-              <li>$WAGYU to be used within merch store.</li>
-            </ul>
-          </div>
-          <div className="card">
-            <h2 className="sub">Future $WAGYU mints</h2>
-            <ul className="story-div">
-              <li>Use $WAGYU for future mints and auctions.</li>
-            </ul>
-          </div>
-          <div className="card">
-            <h2 className="sub">$WAGYU Whitelists & Raffles</h2>
-            <ul className="story-div">
-              <li>Purchase Whitelists of selected projects.</li>
-              <li>Use for regular Raffles.</li>
-            </ul>
-          </div>
+        <img src={img} alt="" className="util-img" />
+        <div className="card card1">
+          <h2 className="sub">
+            Foodie, Chill Out Manager, Star, Celeb Head Chef
+          </h2>
+          <ul className="story-div">
+            <li>Collect $WAGYU & exchange it for your Steaks House.</li>
+            <li>Use $WAGYU to decorate your Steak House.</li>
+          </ul>
+        </div>
+        <div className="card card2">
+          <h2 className="sub">Rancher</h2>
+          <ul className="story-div">
+            <li>Collect $WAGYU & exchange it for your Farm.</li>
+            <li>Use $WAGYU to decorate your Farm.</li>
+          </ul>
+        </div>
+        <div className="card card3">
+          <h2 className="sub">IRL Utility</h2>
+          <ul className="story-div">
+            <li>$WAGYU as only ticket to IRL events.</li>
+            <li>Discounts at selected Steaks Houses</li>
+          </ul>
+        </div>
+        <div className="card card4">
+          <h2 className="sub">$WAGYU Merch</h2>
+          <ul className="story-div">
+            <li>$WAGYU to be used within merch store.</li>
+          </ul>
+        </div>
+
+        <div className="card card5">
+          <h2 className="sub">Future $WAGYU mints</h2>
+          <ul className="story-div">
+            <li>Use $WAGYU for future mints and auctions.</li>
+          </ul>
+        </div>
+
+        <div className="card card6">
+          <h2 className="sub">$WAGYU Whitelists & Raffles</h2>
+          <ul className="story-div">
+            <li>Purchase Whitelists of selected projects.</li>
+            <li>Use for regular Raffles.</li>
+          </ul>
         </div>
       </div>
     </div>
