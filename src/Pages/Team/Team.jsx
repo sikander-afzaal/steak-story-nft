228e6f7cda54da1ef9d0f0c8382b6f3a262818af
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-03-16 23:50:56 +0500

    first commit

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -4,6 +4,8 @@ import teamImg1 from "../../Assets/Team/team (1).png";
 import teamImg2 from "../../Assets/Team/team (2).png";
 import teamImg3 from "../../Assets/Team/team (3).png";
 import teamImg4 from "../../Assets/Team/team (4).png";
+import teamImg5 from "../../Assets/Team/team (5).png";
+import teamImg6 from "../../Assets/Team/team (6).png";
 import head from "../../Assets/Team/team-head.png";
 import headMob from "../../Assets/Team/team-head-mob.png";
 function Team() {
@@ -20,8 +22,8 @@ function Team() {
         <img src={teamImg3} alt="" className="team-img team-img2" />
         <img src={teamImg2} alt="" className="team-img team-img3" />
         <img src={teamImg4} alt="" className="team-img team-img4" />
-        <img src={teamImg4} alt="" className="team-img team-img5" />
-        <img src={teamImg4} alt="" className="team-img team-img6" />
+        <img src={teamImg5} alt="" className="team-img team-img5" />
+        <img src={teamImg6} alt="" className="team-img team-img6" />
       </div>
     </div>
   );
