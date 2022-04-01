573c7d31eeaea72267580a459858d6d5488aacbc
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-01 15:12:50 +0500

    first commit

--- src/Pages/Roadmap/Roadmap.jsx
+++ src/Pages/Roadmap/Roadmap.jsx
@@ -16,27 +16,31 @@ function Roadmap() {
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road1" />
               <p className="abs-text-road">
-                Art creation | Build a foodie community
+                Art creation | Building a true foodie community | Brand building
               </p>
             </div>
           </div>
           <div className="circle-road-div2">
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road2" />
-              <p className="abs-text-road">SolSteaks mint</p>
+              <p className="abs-text-road">SolSteaks mint day 🥩</p>
             </div>
           </div>
           <div className="circle-road-div3">
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road3" />
-              <p className="abs-text-road">Steak & collect $WAGYU</p>
+              <p className="abs-text-road">
+                Staking with 5 roles | Steak & collect $WAGYU | Interesting role
+                based tokenomics{" "}
+              </p>
             </div>
           </div>
           <div className="circle-road-div4">
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road4" />
               <p className="abs-text-road">
-                Exchange $WAGYU for Steaks House NFT or Rancher NFT
+                Exchange $WAGYU for Steaks House NFT or Rancher NFT depending on
+                your role | Risk factor involved
               </p>
             </div>
           </div>
@@ -44,21 +48,23 @@ function Roadmap() {
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road5" />
               <p className="abs-text-road">
-                IRL events | Discounts in selected Steaks Houses
+                Exclusive IRL events | Discounts in selected Steaks Houses
               </p>
             </div>
           </div>
           <div className="circle-road-div6">
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road6" />
-              <p className="abs-text-road">Community Cook Book</p>
+              <p className="abs-text-road">
+                Community Cook Book with recipes around the World{" "}
+              </p>
             </div>
           </div>
           <div className="circle-road-div7">
             <div className="wrapper-img">
               <img src={circle} alt="" className="circle-road road7" />
               <p className="abs-text-road">
-                Launch of SolSteaks merch & food products
+                Launch of SolSteaks merch | SolSteaks food products
               </p>
             </div>
           </div>
